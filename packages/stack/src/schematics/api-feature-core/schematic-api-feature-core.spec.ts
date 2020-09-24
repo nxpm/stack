3e30dd002acc2dde9cafa3338214c2daff826816
Bram Borggreve <borggreve@gmail.com>
2020-09-24 07:57:18 -0500

    feat: add api-feature-core library

---
+++ packages/stack/src/schematics/api-feature-core/schematic-api-feature-core.spec.ts
@@ -0,0 +1,29 @@
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { createEmptyWorkspace } from '@nrwl/workspace/testing';
+import { join } from 'path'
+
+import { ApiFeatureCoreSchematicSchema } from './schema';
+
+describe('api-feature-core schematic', () => {
+  let appTree: Tree;
+  const options: ApiFeatureCoreSchematicSchema = { name: 'test' };
+
+  const testRunner = new SchematicTestRunner(
+    '@nxpm/api-feature-core',
+    join(__dirname, '../../../collection.json')
+  );
+
+  beforeEach(() => {
+    appTree = createEmptyWorkspace(Tree.empty());
+  });
+
+  it('should run successfully', async () => {
+    await expect(testRunner.runSchematicAsync(
+        'api-feature-core',
+        options,
+        appTree
+      ).toPromise()
+    ).resolves.not.toThrowError();
+  })
+});
