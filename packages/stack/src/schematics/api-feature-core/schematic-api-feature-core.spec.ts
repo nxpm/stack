619b16b8aa22c50b00b59982c57b5f3323a5067a
Bram Borggreve <borggreve@gmail.com>
2020-09-24 08:02:02 -0500

    chore: fix linting errors

--- packages/stack/src/schematics/api-feature-core/schematic-api-feature-core.spec.ts
+++ packages/stack/src/schematics/api-feature-core/schematic-api-feature-core.spec.ts
@@ -1,29 +1,23 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import { createEmptyWorkspace } from '@nrwl/workspace/testing';
+import { Tree } from '@angular-devkit/schematics'
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing'
+import { createEmptyWorkspace } from '@nrwl/workspace/testing'
 import { join } from 'path'
 
-import { ApiFeatureCoreSchematicSchema } from './schema';
+import { ApiFeatureCoreSchematicSchema } from './schema'
 
 describe('api-feature-core schematic', () => {
-  let appTree: Tree;
-  const options: ApiFeatureCoreSchematicSchema = { name: 'test' };
+  let appTree: Tree
+  const options: ApiFeatureCoreSchematicSchema = { name: 'test', type: 'feature' }
 
-  const testRunner = new SchematicTestRunner(
-    '@nxpm/api-feature-core',
-    join(__dirname, '../../../collection.json')
-  );
+  const testRunner = new SchematicTestRunner('@nxpm/api-feature-core', join(__dirname, '../../../collection.json'))
 
   beforeEach(() => {
-    appTree = createEmptyWorkspace(Tree.empty());
-  });
+    appTree = createEmptyWorkspace(Tree.empty())
+  })
 
   it('should run successfully', async () => {
-    await expect(testRunner.runSchematicAsync(
-        'api-feature-core',
-        options,
-        appTree
-      ).toPromise()
-    ).resolves.not.toThrowError();
+    await expect(
+      testRunner.runSchematicAsync('api-feature-core', options, appTree).toPromise(),
+    ).resolves.not.toThrowError()
   })
-});
+})
