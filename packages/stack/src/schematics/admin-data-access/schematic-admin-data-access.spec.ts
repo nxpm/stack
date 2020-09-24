1f9f6210ca713450741d1295251054940b28fc0e
Bram Borggreve <borggreve@gmail.com>
2020-09-24 08:02:02 -0500

    chore: fix linting errors

--- packages/stack/src/schematics/admin-data-access/schematic-admin-data-access.spec.ts
+++ packages/stack/src/schematics/admin-data-access/schematic-admin-data-access.spec.ts
@@ -1,29 +1,23 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import { createEmptyWorkspace } from '@nrwl/workspace/testing';
+import { Tree } from '@angular-devkit/schematics'
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing'
+import { createEmptyWorkspace } from '@nrwl/workspace/testing'
 import { join } from 'path'
 
-import { AdminDataAccessSchematicSchema } from './schema';
+import { AdminDataAccessSchematicSchema } from './schema'
 
 describe('admin-data-access schematic', () => {
-  let appTree: Tree;
-  const options: AdminDataAccessSchematicSchema = { name: 'test' };
+  let appTree: Tree
+  const options: AdminDataAccessSchematicSchema = { name: 'test' }
 
-  const testRunner = new SchematicTestRunner(
-    '@nxpm/admin-data-access',
-    join(__dirname, '../../../collection.json')
-  );
+  const testRunner = new SchematicTestRunner('@nxpm/admin-data-access', join(__dirname, '../../../collection.json'))
 
   beforeEach(() => {
-    appTree = createEmptyWorkspace(Tree.empty());
-  });
+    appTree = createEmptyWorkspace(Tree.empty())
+  })
 
   it('should run successfully', async () => {
-    await expect(testRunner.runSchematicAsync(
-        'admin-data-access',
-        options,
-        appTree
-      ).toPromise()
-    ).resolves.not.toThrowError();
+    await expect(
+      testRunner.runSchematicAsync('admin-data-access', options, appTree).toPromise(),
+    ).resolves.not.toThrowError()
   })
-});
+})
