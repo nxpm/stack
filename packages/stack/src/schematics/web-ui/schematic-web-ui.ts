116825390a10ac9c8e575b53ff42ede6f18d3e2b
Bram Borggreve <borggreve@gmail.com>
2021-02-11 02:16:16 -0500

    fix: make sure web-ui creates working libs

--- packages/stack/src/schematics/web-ui/schematic-web-ui.ts
+++ packages/stack/src/schematics/web-ui/schematic-web-ui.ts
@@ -1,29 +1,35 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics'
 import { ProjectType } from '@nrwl/workspace'
-import { addFiles, createProjectName, normalizeOptions } from '../../utils'
+import { addFiles, createProjectName, getPluginConfig, normalizeOptions } from '../../utils'
 import { WebUiSchematicSchema } from './schema'
+import { strings } from '@angular-devkit/core'
 
 export default function (options: WebUiSchematicSchema): Rule {
+  const plugin = getPluginConfig(options)
   const type = 'ui'
-  const directory = options.directory || 'web'
+  const directory = options.directory || strings.dasherize(plugin.webAppName)
   const name = createProjectName(options.name, type, true)
   const style = options.style || 'css'
 
   const normalizedOptions = normalizeOptions<WebUiSchematicSchema>({ ...options, directory, name }, ProjectType.Library)
+  const templateOptions = {
+    name: options.name,
+    prefix: options.prefix || 'ui',
+  }
 
+  console.log({ normalizedOptions })
   return chain([
     externalSchematic('@nrwl/angular', 'library', {
-      name,
-      directory,
+      ...templateOptions,
+      directory: `${normalizedOptions.directory}/${type}`,
       tags: `scope:${directory},type:${type}`,
       style,
-      prefix: options.prefix || options.name,
       buildable: options.buildable || false,
       publishable: options.publishable || false,
       routing: true,
       lazy: true,
       linter: 'eslint',
     }),
-    addFiles(normalizedOptions),
+    addFiles({ ...normalizedOptions, ...templateOptions }),
   ])
 }
--- packages/stack/src/utils/index.ts
+++ packages/stack/src/utils/index.ts
@@ -180,6 +180,7 @@ export function normalizeOptions<T extends BaseSchema>(options: T, projectType: 
     parsedTags,
     apiAppName,
     webAppName,
+    name,
   }
 }
 
