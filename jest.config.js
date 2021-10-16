868637cccb51adda2efce585554c385cf2889dae
Bram Borggreve <borggreve@gmail.com>
2021-10-16 12:34:08 -0500

    chore: update Nx to latest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
+const { getJestProjects } = require('@nrwl/jest')
+
 module.exports = {
   projects: [
-    '<rootDir>/packages/stack',
+    ...getJestProjects(),
     '<rootDir>/e2e/stack-e2e',
-    '<rootDir>/packages/api',
     '<rootDir>/e2e/api-e2e',
-    '<rootDir>/packages/web',
     '<rootDir>/e2e/web-e2e',
-    '<rootDir>/packages/mobile',
     '<rootDir>/e2e/mobile-e2e',
-    '<rootDir>/packages/common',
   ],
 }
