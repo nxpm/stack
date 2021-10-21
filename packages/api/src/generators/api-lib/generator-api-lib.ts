fd0ac7ec8da11abed7311662c478d1f582e5db91
Bram Borggreve <borggreve@gmail.com>
2021-10-20 22:30:11 -0500

    feat: expose extraOptions in generateApiLib and generatorWebLib

--- packages/api/src/generators/api-lib/generator-api-lib.ts
+++ packages/api/src/generators/api-lib/generator-api-lib.ts
@@ -3,14 +3,19 @@ import { libraryGenerator } from '@nrwl/nest'
 import { addFiles, normalizeOptions } from '@nxpm/common'
 import { ApiLibGeneratorSchema } from './schema'
 
-export async function generatorApiLib(host: Tree, options: ApiLibGeneratorSchema, files?: null) {
+export async function generatorApiLib(
+  host: Tree,
+  options: ApiLibGeneratorSchema,
+  files?: string,
+  extraOptions: Record<string, string> = {},
+) {
   const normalizedOptions = normalizeOptions(host, options)
   await libraryGenerator(host, {
     ...options,
     name: options.name,
   })
   if (files) {
-    addFiles(host, normalizedOptions, files)
+    addFiles(host, normalizedOptions, files, extraOptions)
   }
   await formatFiles(host)
 }
--- packages/web/src/generators/web-lib/generator-web-lib.ts
+++ packages/web/src/generators/web-lib/generator-web-lib.ts
@@ -1,16 +1,21 @@
-import { formatFiles, Tree } from '@nrwl/devkit'
 import { libraryGenerator } from '@nrwl/angular/generators'
+import { formatFiles, Tree } from '@nrwl/devkit'
 import { addFiles, normalizeOptions } from '@nxpm/common'
 import { WebLibGeneratorSchema } from './schema'
 
-export async function generatorWebLib(host: Tree, options: WebLibGeneratorSchema, files?: null) {
+export async function generatorWebLib(
+  host: Tree,
+  options: WebLibGeneratorSchema,
+  files?: string,
+  extraOptions: Record<string, string> = {},
+) {
   const normalizedOptions = normalizeOptions(host, options)
   await libraryGenerator(host, {
     ...options,
     name: options.name,
   })
   if (files) {
-    addFiles(host, normalizedOptions, files)
+    addFiles(host, normalizedOptions, files, extraOptions)
   }
   await formatFiles(host)
 }
