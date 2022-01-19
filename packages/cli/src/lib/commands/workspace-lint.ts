cece0058e00ffc81982fa2fa28732363d2a1b91a
Kevin <kpaxton@paxdesigns.com>
2022-01-19 10:42:19 -0500

    Remove unecessary sorts, add undefined check to packageJson sort

--- packages/cli/src/lib/commands/workspace-lint.ts
+++ packages/cli/src/lib/commands/workspace-lint.ts
@@ -17,46 +17,14 @@ function sortItems(items: Record<string, unknown> = {}) {
     .reduce((acc, curr) => ({ ...acc, [curr]: items[curr] }), {})
 }
 
-export function sortTsConfigPaths(file) {
-  return {
-    ...file,
-    compilerOptions: {
-      ...file?.compilerOptions,
-      paths: sortItems(file?.compilerOptions?.paths),
-    },
-  }
-}
-
-export function sortWorkspaceProjects(file) {
-  return {
-    ...file,
-    projects: sortItems(file?.projects),
-  }
-}
-
 export function sortPackageJson(file) {
   return {
     ...file,
-    scripts: sortItems(file?.scripts),
+    ...(file?.scripts ? { scripts: sortItems(file?.scripts) } : {}),
   }
 }
 
 export async function workspaceLint({ dryRun, skipPackageJson }: { dryRun: boolean; skipPackageJson: boolean }) {
-  for (const file of projectFiles) {
-    const contents = await getFileContents(file)
-    if (contents && !dryRun) {
-      await writeJson(file, sortWorkspaceProjects(contents), { spaces: 2 })
-      execSync('prettier --write ' + file)
-    }
-  }
-
-  for (const file of tsconfigFiles) {
-    const contents = await getFileContents(file)
-    if (contents && !dryRun) {
-      await writeJson(file, sortTsConfigPaths(contents), { spaces: 2 })
-      execSync('prettier --write ' + file)
-    }
-  }
   if (!skipPackageJson) {
     const file = 'package.json'
     const contents = await getFileContents(file)
